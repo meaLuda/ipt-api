31d65a3b4a118ee29ddd7422378429471676d7fc
meaLuda <munyalamea@gmail.com>
2022-11-09 12:14:01 +0300

    update tools library

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
-# IMAGE PREPROCESSING AS A SERVICE TOOL FOR BUILDERS | DEVELOPERS (WebApp). 
+# IMAGE PREPROCESSING AS A SERVICE TOOL FOR BUILDERS | DEVELOPERS (WebApp)
 
 ```text
 The aim of this project is to create a plug-and-play API-as-a-service for 
 image preprocessing and later on machine learning and deep-learning too
+```
+
+---
+
+```R
+TODO:                                               Deadline        Done? 
+- Create Image preprocessing tools (2 to 3)        11/02/22         [  ]         
+
+```
+
+---
+
+<h1> Helper Links & Notes </h1>
+
+[Creating a Data Pipeline with EasyJobs & FastAPI](https://medium.com/analytics-vidhya/creating-a-data-pipeline-with-easyjobs-fastapi-4e302556f05d#:~:text=Creating%20a%20Data%20Pipeline%20with%20EasyJobs%20%26%20FastAPI,...%207%20Task%20Flow%20...%208%20Conclusion%20)
+
+[ML Pipeline Tutorial](https://www.youtube.com/watch?v=CApCQKuWqBM&ab_channel=CodingEntrepreneurs)
+
+[How you can quickly deploy your ML models with FastAPI](https://towardsdatascience.com/how-you-can-quickly-deploy-your-ml-models-with-fastapi-9428085a87bf)
+
+--- 
+
+<h2> Code Snippets </h2>
+
+```python
+# ?? should we consider chunking of > 100 images | multithreading rather than a loop. ??
+images = [file for file in os.listdir() if file.endswith(('jpeg', 'png', 'jpg'))]
+for image in images:
+    img = Image.open(image)
+    # image manipulation functions/functionalities.
+    img.thumbnail((600,600))
+    img.save("resized_"+image, optimize=True, quality=40)
 ```
--- tools/resize/resize.py
+++ tools/resize/resize.py
@@ -1,2 +1,80 @@
 import opencv
+from PIL import Image, ImageOps
+import numpy as np
+from numpy import asarray
+"""
+## Class to Resize images using Nearest Neighbour Algorith:
 
+# Inputs:
+    image_array, image_width, image_height
+# Outputs
+    new_image_array, new_img_width,new_image_height
+
+"""
+
+
+# Functions
+# def myImageResize(img_array,new_w,new_h,interpoltn):
+#     """
+#         # Experiment 1: Downsample then upsample using nearest neighbor interpolation.
+#     """
+#     if interpoltn == 'nearest':
+#         new_img = np.array(Image.fromarray(img_array).resize((new_h, new_w), Image.Resampling.NEAREST))
+#         from matplotlib import pyplot as plt
+#         plt.title(f"Downsampled To Width:{new_w} Height:{new_h} using NN  interpolation") 
+#         plt.imshow(new_img,  interpolation='nearest')
+#         plt.show()
+
+def myImageResize(img_array,new_w,new_h,interpoltn):
+    """
+        # Experiment 1: Downsample then upsample using nearest neighbor interpolation.
+    """
+    if interpoltn == 'nearest':
+        new_img_nearest = np.array(Image.fromarray(img_array).resize((new_h, new_w), Image.Resampling.NEAREST))
+        return new_img_nearest
+    
+    if interpoltn == 'bilinear':
+        new_img_bilinear = np.array(Image.fromarray(img_array).resize((new_h, new_w), Image.Resampling.BILINEAR))
+        return new_img_bilinear
+
+def mybilinear(img_array,new_w,new_h,interpoltn):
+    img_bilinear = np.array(Image.fromarray(img_array).resize((new_h, new_w), Image.Resampling.BILINEAR))
+    return img_bilinear
+
+
+def myRMSE(orig_array, up_down_sampled_array):
+    """
+        # Subtract new array from old array then sqr and get the mean
+    """
+    return np.sqrt(((up_down_sampled_array - orig_array) ** 2).mean())
+
+
+class ResizeTool:
+    def __init__(self,img_array):
+        """
+            #Assumptions:
+                We have used opencv to read the image.
+                .i.e. 
+                import cv2
+
+                im = cv2.imread('data/src/lena.jpg')
+
+                print(type(im))
+                # <class 'numpy.ndarray'>
+
+                print(im.shape)
+                print(type(im.shape))
+                # (225, 400, 3)
+                # <class 'tuple'>
+        """
+        self.image_array = img_array
+    
+    def w_h(self):
+        # return previouse image width,height and channel
+        w,h,channel = self.image_array[:,2]
+        return (w,h,channel)
+
+    def resize(self,new_w,new_h):
+        
+        new_img_nearest = np.array(Image.fromarray(self.image_array).resize((new_h, new_w), Image.Resampling.NEAREST))
+        return new_img_nearest
