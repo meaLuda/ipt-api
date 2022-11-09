6992a82e32bccfdcbcd5031386ad3ced08691fcc
meaLuda <munyalamea@gmail.com>
2022-11-09 12:57:47 +0300

    -

--- tools/resize/resize.py
+++ tools/resize/resize.py
@@ -1,7 +1,10 @@
-import opencv
+import cv2
 from PIL import Image, ImageOps
 import numpy as np
 from numpy import asarray
+from typing import Union,List
+import matplotlib.pyplot as plt
+
 """
 ## Class to Resize images using Nearest Neighbour Algorith:
 
@@ -11,43 +14,6 @@ from numpy import asarray
     new_image_array, new_img_width,new_image_height
 
 """
-
-
-# Functions
-# def myImageResize(img_array,new_w,new_h,interpoltn):
-#     """
-#         # Experiment 1: Downsample then upsample using nearest neighbor interpolation.
-#     """
-#     if interpoltn == 'nearest':
-#         new_img = np.array(Image.fromarray(img_array).resize((new_h, new_w), Image.Resampling.NEAREST))
-#         from matplotlib import pyplot as plt
-#         plt.title(f"Downsampled To Width:{new_w} Height:{new_h} using NN  interpolation") 
-#         plt.imshow(new_img,  interpolation='nearest')
-#         plt.show()
-
-def myImageResize(img_array,new_w,new_h,interpoltn):
-    """
-        # Experiment 1: Downsample then upsample using nearest neighbor interpolation.
-    """
-    if interpoltn == 'nearest':
-        new_img_nearest = np.array(Image.fromarray(img_array).resize((new_h, new_w), Image.Resampling.NEAREST))
-        return new_img_nearest
-    
-    if interpoltn == 'bilinear':
-        new_img_bilinear = np.array(Image.fromarray(img_array).resize((new_h, new_w), Image.Resampling.BILINEAR))
-        return new_img_bilinear
-
-def mybilinear(img_array,new_w,new_h,interpoltn):
-    img_bilinear = np.array(Image.fromarray(img_array).resize((new_h, new_w), Image.Resampling.BILINEAR))
-    return img_bilinear
-
-
-def myRMSE(orig_array, up_down_sampled_array):
-    """
-        # Subtract new array from old array then sqr and get the mean
-    """
-    return np.sqrt(((up_down_sampled_array - orig_array) ** 2).mean())
-
 
 class ResizeTool:
     def __init__(self,img_array):
@@ -75,6 +41,33 @@ class ResizeTool:
         return (w,h,channel)
 
     def resize(self,new_w,new_h):
-        
         new_img_nearest = np.array(Image.fromarray(self.image_array).resize((new_h, new_w), Image.Resampling.NEAREST))
         return new_img_nearest
+
+    
+    def debug_class(self,debug=False):
+        """ 
+            # A debug function for tools we are creating
+            # A plt showing before img and after tool process is complete.
+            #  
+        """
+        # BGR to RGB and flip frame
+        # input_image = cv2.imread(local_img_path)
+        
+        input_image  = self.image_array
+        new_image_array = self.resize(500,500)
+        #subplot(r,c) provide the no. of rows and columns
+        f, axarr = plt.subplots(2,2) 
+        # To debug what is actually fed to network
+        if debug:
+            for ax in axarr:
+                # use the created array to output your multiple images. In this case I have stacked 4 images vertically
+                ax.imshow(input_image, interpolation='none')
+                ax.imshow(new_image_array, interpolation='none')
+                plt.show(block=True)
+        
+
+if __name__ == "__main__":
+    im = cv2.imread("C:/Users/munya/Desktop/Projects/ipt-api-apias/ipt-api/tools/resize/dappod.jpg")
+    test = ResizeTool(im)
+    test.debug_class(debug=True)
