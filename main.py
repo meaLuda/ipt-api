c9548916d51b50d205f68b5dab208b5c1fe4361c
meaLuda <munyalamea@gmail.com>
2022-11-30 16:33:42 +0300

    added testing folder with notebook

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
+# ------------ Documentation --------
+# some documentation in markdown
+description = """  
+## Documentation
+**TODO: Read carefully before using**
+
+This api allows you to perform basic image,video,and audio functions over the web be it in your app or webapp.  
+We will also include imaging capabilitied over the web.... 
+
+
+
+_Build credits:_
+
+```text
+    *Python imageio
+    *Huggingface Models
+```
+
+___
+
+## To run locally: ```$uvicorn app:main --reload```
+
+"""
+
+# -----------------------------------
+
 from typing import Union
 from fastapi import FastAPI
 
 
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello":"World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+app = FastAPI(
+    title="IPT API FOR Quick [image,audio,vedio] editing & ML Models",
+    description=description,
+    version="0.0.0",
+    contact={
+        "name": "Munyala Eliud | Paul Ndirangu",
+        "emails": "<munyalamea@gmail.com> , <>",
+    },
+)
+
+@app.get("/welcome_test/{test}")
+def read_root(test:str):
+    return {"test_info_success":test}
+
+
